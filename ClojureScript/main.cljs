d93e83284403c297b874ba25c85471717e29b937
Maciej Szreder <mszreder3.14@wp.pl>
2025-07-17 03:36:48 +0200

    convert key to binary digit position

--- ClojureScript/main.cljs
+++ ClojureScript/main.cljs
@@ -1,3 +1,17 @@
-(.addEventListener js/document "keydown" #(
-	js/console.log "Key down event detected" (.-key %)
-))
+(do
+	(def encodeKey {
+		"q" 1
+		"w" 2
+		"e" 4
+		"f" 8
+		"c" 16
+		"m" 32
+		"j" 64
+		"i" 128
+		"o" 256
+		"p" 512
+	})
+	(.addEventListener js/document "keydown" #(
+		js/console.log "Key down event detected" (get encodeKey (.-key %))
+	))
+)
